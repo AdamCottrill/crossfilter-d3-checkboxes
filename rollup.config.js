68fb8c86e0986b044da4999d51f486cb14f0f3d6
Adam Cottrill <racottrill@gmail.com>
2021-03-13 08:40:23 -0500

    Upgrade dependancies and rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,58 +1,54 @@
-import babel from 'rollup-plugin-babel';
-import { eslint } from 'rollup-plugin-eslint';
-import resolve  from 'rollup-plugin-node-resolve';
-import json from 'rollup-plugin-json';
-import commonjs from 'rollup-plugin-commonjs';
-import uglify from 'rollup-plugin-uglify-es';
-import replace from 'rollup-plugin-replace';
+import { babel } from "@rollup/plugin-babel";
+import eslint from "@rollup/plugin-eslint";
+import resolve from "@rollup/plugin-node-resolve";
+import json from "@rollup/plugin-json";
+import commonjs from "@rollup/plugin-commonjs";
+import replace from "@rollup/plugin-replace";
+import { terser } from "rollup-plugin-terser";
 
-
-function onwarn (warning, warn) {
+function onwarn(warning, warn) {
   // skip certain warnings
-  if (warning.code === 'CIRCULAR_DEPENDENCY') return;
+  if (warning.code === "CIRCULAR_DEPENDENCY") return;
   // Use default for everything else
   warn(warning);
 }
 
 export default {
-  input: 'src/js/main.js',
+  input: "src/js/main.js",
   onwarn: onwarn,
 
   output: {
-    name: 'MyModule',
-    file: 'bundle.js',
-    format: 'iife',
-    sourceMap: 'inline',
+    name: "MyModule",
+    file: "bundle.js",
+    format: "iife",
+    sourceMap: "inline",
     globals: {
-      'd3': 'd3',
-      'crossfilter2': 'crossfilter'
-    }
+      d3: "d3",
+      crossfilter2: "crossfilter",
+    },
   },
 
   plugins: [
     resolve({
       jsnext: true,
       main: true,
-      browser: true
+      browser: true,
     }),
     json(),
     commonjs(),
     eslint({
-      include: ['src/js/**'],
-      exclude: ['src/css/**']
+      include: ["src/js/**"],
+      exclude: ["src/css/**"],
     }),
 
     replace({
-      ENV: JSON.stringify(process.env.NODE_ENV || 'development')
+      preventAssignment: true,
+      ENV: JSON.stringify(process.env.NODE_ENV || "development"),
     }),
 
-    (process.env.NODE_ENV === 'production' && uglify()),
-
+    process.env.NODE_ENV === "production" && terser(),
     babel({
-      exclude: 'node_modules/**',
+      exclude: "node_modules/**",
     }),
-
-
-
-  ]
+  ],
 };
